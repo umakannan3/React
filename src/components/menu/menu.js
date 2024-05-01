71259a5ce9d3a7d0e759d37f26cdb40ab05d8e42
umakannan3 <umaheshcbe3@gmail.com>
2024-05-02 01:03:29 +0530

    changes

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -6,7 +6,7 @@ export function Menu(){
        
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-info">
             <div class="container-fluid">
-            <Link class="navbar-brand" href="#">Moodle Assessment</Link>
+            {/* <Link class="navbar-brand" href="#">Moodle Assessment</Link> */}
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
